d7406221a56b1e4c98fce81a55c1ac2fad632db4
tshepoandi <lovekilledtshepo@gmail.com>
2023-03-20 10:43:24 +0200

    pyramid array

---
+++ pyramidArray.rb
@@ -0,0 +1,26 @@
+# https://www.codewars.com/kata/515f51d438015969f7000013/train/ruby
+
+
+def pyramid(number)
+ answer = []
+ if number < 1
+   return []
+ else
+   number.times do |i|
+     iteration = i + 1
+     array = "1" * iteration
+     array = array.split("").map { |num| num.to_i}
+     answer.push(array)
+   end
+ end
+  return answer
+end
+
+
+
+
+
+#    make a loop
+#    for every iteration repeat character and split it
+#    map every split to interger
+#    boom
