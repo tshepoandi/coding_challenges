6a810970a77b8863f4e3bf7be261bb14a4de7ac6
tshepoandi <lovekilledtshepo@gmail.com>
2023-04-03 11:22:09 +0200

    simple Pig Latin

---
+++ simplePigLating.rb
@@ -0,0 +1,21 @@
+# https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/ruby
+
+def pig_it(text)
+  return(
+    text
+      .split(" ")
+      .map do |i|
+        if i[i.length - 1] =~ /\w/
+          i[1..i.length] + i[0] + "ay"
+        else
+          if i[0] =~ /w/
+            wordLength = i.length - 2
+            i[1..wordLength] + i[0] + "ay"
+          else
+            i
+          end
+        end
+      end
+      .join(" ")
+  )
+end
