ee01bfa1d0b88bb02d4d715aae71bcb09b029f73
tshepo <lovekilledtshepo@gmail.com>
2023-06-02 10:03:03 +0200

    detectParagram

---
+++ detectPangram.js
@@ -0,0 +1,31 @@
+// https://www.codewars.com/kata/545cedaa9943f7fe7b000048/train/javascript
+
+function isPangram(string){
+    let alphabets = "abcdefghijklmnopqrstuvwxyz".toLowerCase().split("")
+    string.split("").forEach((letter)=>{
+        ind = alphabets.indexOf(letter)
+        if (ind !== -1){
+            alphabets.splice(ind,1)
+        }
+    })
+    if (alphabets.length == 0){
+        return true
+    }
+    return false
+}
+
+console.log(isPangram("The quick brown fo jumps over the lazy dog"))
+console.log(isPangram("Cwm fjord bank glyphs vext quiz"))
+console.log(isPangram("Pack my box with five dozen liquor jugs."))
+console.log(isPangram("How quickly daft jumping zebras vex"))
+// const array = [2, 5, 9];
+
+// console.log(array);
+
+// const index = array.indexOf(5);
+// if (index > -1) { // only splice array when item is found
+//   array.splice(index, 1); // 2nd parameter means remove one item only
+// }
+
+// // array = [2, 9]
+// console.log(array); 
