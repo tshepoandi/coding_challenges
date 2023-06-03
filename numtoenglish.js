274e91038c7a76f13d08c5803ec761038d2cecc5
tshepo <lovekilledtshepo@gmail.com>
2023-06-03 15:10:05 +0200

    untill 9999

--- numtoenglish.js
+++ numtoenglish.js
@@ -4,24 +4,41 @@
 
 const numberToEnglish = n => {
     dictionary = {
-        singles:["one","two","three","four","five","six","seven","eight","nine"],
+        singles:["zero","one","two","three","four","five","six","seven","eight","nine"],
         lessThanTwenty:["eleven","twelve","thirteen","forteen","fifteen","sixteen","seventeen","eighteen","nineteen"],
-        2:["ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"],      
+        tens:["ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"], 
+        0:"zero",
+        1:"one",
+        2:"two",     
         3:"hundred",
         4:"thousand",
     }
     let number = n.toString().split("")
     let numberLength = number.length
     const numArr = []
+    let answer = []
     for (let i = 0;i < number.length;i++){
         numberLength -= 1
         numArr.push(number[i] + "0".repeat(numberLength))
-        // if numArr.push(dictionary)
+        let fill;
+        if (numArr[i].length == 1) {
+            fill = dictionary.singles[parseInt(numArr[i][0])] !== "zero" ? dictionary.singles[parseInt(numArr[i][0])] : "";
+            answer.push(fill)
+            break
+        }
+        if (numArr[i].length == 2){
+            fill = dictionary.tens[parseInt(numArr[i][0]) -1]
+            answer.push(fill)
+            continue
+        }
+        if(numArr[i].length >= 3)  fill = dictionary[numArr[i].length]
+
+        answer.push(dictionary.singles[[parseInt(numArr[i][0])]] + " " + fill)
     }
     // let ans;
   
-    // return ans
+    return [numArr,answer.join(" ")]
 }
 
-console.log(numberToEnglish(540))
+console.log(numberToEnglish(1541))
 console.log(numberToEnglish(23))
