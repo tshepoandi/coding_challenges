c6b2c9c214b3354e93a54b203729dd0ae08f571e
tshepo <lovekilledtshepo@gmail.com>
2023-06-03 09:19:09 +0200

    detectpan

--- detectPangram.js
+++ detectPangram.js
@@ -1,22 +1,25 @@
 // https://www.codewars.com/kata/545cedaa9943f7fe7b000048/train/javascript
 
 function isPangram(string){
-    let alphabets = "abcdefghijklmnopqrstuvwxyz".toLowerCase().split("")
+    let alphabets = "abcdefghijklmnopqrstuvwxyz".split("")
     string.split("").forEach((letter)=>{
+        letter = letter.toLowerCase()
         ind = alphabets.indexOf(letter)
         if (ind !== -1){
+
             alphabets.splice(ind,1)
         }
     })
+    console.log(alphabets)
     if (alphabets.length == 0){
         return true
     }
     return false
 }
 
-console.log(isPangram("The quick brown fo jumps over the lazy dog"))
-console.log(isPangram("Cwm fjord bank glyphs vext quiz"))
-console.log(isPangram("Pack my box with five dozen liquor jugs."))
+// console.log(isPangram("The quick brown fox jumps over the lazy dog"))
+// console.log(isPangram("Cwm fjord bank glyphs vext quiz"))
+// console.log(isPangram("Pack my box with five dozen liquor jugs."))
 console.log(isPangram("How quickly daft jumping zebras vex"))
 // const array = [2, 5, 9];
 
