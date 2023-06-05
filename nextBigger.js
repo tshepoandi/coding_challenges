f12b87e11202135460037587ea53227f2face91c
tshepo <lovekilledtshepo@gmail.com>
2023-06-05 08:46:29 +0200

    nextBigger---not done

--- nextBigger.js
+++ nextBigger.js
@@ -9,15 +9,44 @@ const nextBigger = n => {
     else{
         for(let i = 0;i < nString.length;i++){
             let newNumber = nString[nString.length - 1 - i]
-            console.log(nString,newNumber)
+            for(let i=-1; i < nString.length;i++ ){
+                swapElements(nString,nString.length - i, newNumber)
+                nString = nString.filter((element)=>{
+                    if(element !== undefined) return element
+                })
+                console.log(parseInt(nString.join("")),n)
+                if(parseInt(nString.join("")) > n ) return parseInt(nString.join(""))
+            }
         }
-        // return nString[nString.length -1]
+        return -1
     }
 }
-console.log(nextBigger(1))
-console.log(nextBigger(21))
-console.log(nextBigger(12))
-console.log(nextBigger(531))
-console.log(nextBigger(513))
+// console.log(nextBigger(1))
+// console.log(nextBigger(21))
+// console.log(nextBigger(12))
+console.log(nextBigger(151))
+// console.log(nextBigger(513))
 // console.log(nextBigger(21))
 // tillTommorow
+
+function swapElements(arr, i1, i2) {
+  // Step 1
+  let temp = arr[i1];
+
+  // Step 2
+  arr[i1] = arr[i2];
+
+  // Step 3
+  arr[i2] = temp;
+}
+
+const arr = [1, 2, 3, 4];
+
+swapElements(arr, 2, 3);
+
+// console.log(arr); // [ 4, 2, 3, 1 ]
+
+// 12345
+// 12354
+// 12534
+// 12
