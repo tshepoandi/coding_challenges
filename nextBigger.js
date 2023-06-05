b114b6b6663f8d24c684279f8e9b7087c37fad22
tshepo <lovekilledtshepo@gmail.com>
2023-06-05 09:40:43 +0200

    nextBigger ,91pass 50fails

--- nextBigger.js
+++ nextBigger.js
@@ -1,52 +1,32 @@
+// https://www.codewars.com/kata/55983863da40caa2c900004e/train/javascript
+
 const nextBigger = n => {
-    let nString = n.toString().split("")
-    
-    if (nString.length == 1) return -1
-    if (nString.length == 2 ) {
-        let reversed = parseInt(nString.reverse().join(""))
-        return reversed > n ? reversed : -1
-    }
-    else{
-        for(let i = 0;i < nString.length;i++){
-            let newNumber = nString[nString.length - 1 - i]
-            for(let i=-1; i < nString.length;i++ ){
-                swapElements(nString,nString.length - i, newNumber)
-                nString = nString.filter((element)=>{
-                    if(element !== undefined) return element
-                })
-                console.log(parseInt(nString.join("")),n)
-                if(parseInt(nString.join("")) > n ) return parseInt(nString.join(""))
-            }
+    let nString = n.toString().split("").reverse()
+    for(let i = 0;i < nString.length -1;i++){
+        if(parseInt(nString[i]) > parseInt(nString[i+1])){
+            swapElements(nString,i+1,i)
+            return parseInt(nString.reverse().join(""))
         }
-        return -1
     }
+    return -1
 }
-// console.log(nextBigger(1))
-// console.log(nextBigger(21))
-// console.log(nextBigger(12))
-console.log(nextBigger(151))
-// console.log(nextBigger(513))
-// console.log(nextBigger(21))
-// tillTommorow
+
 
 function swapElements(arr, i1, i2) {
-  // Step 1
   let temp = arr[i1];
-
-  // Step 2
   arr[i1] = arr[i2];
-
-  // Step 3
   arr[i2] = temp;
 }
 
-const arr = [1, 2, 3, 4];
+const arr = [10, 20, 30, 40];
 
 swapElements(arr, 2, 3);
 
-// console.log(arr); // [ 4, 2, 3, 1 ]
+console.log(nextBigger(1))
+console.log(nextBigger(21))
+console.log(nextBigger(12))
+console.log(nextBigger(115))
+console.log(nextBigger(513))
+console.log(nextBigger(2017))
 
-// 12345
-// 12354
-// 12534
-// 12
+
